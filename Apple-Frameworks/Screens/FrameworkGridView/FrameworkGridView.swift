d11ed5927875085feb64461e84c29bd3f228bcf6
Steven Schwab <schwabs@umich.edu>
2022-12-15 19:58:37 -0600

    first commit

---
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -0,0 +1,38 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Steven Schwab on 12/15/22.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: viewModel.columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                viewModel.selectedFramework = framework
+                            }
+                    }
+                }
+            }
+            .navigationTitle("Apple Frameworks")
+            .sheet(isPresented: $viewModel.isShowingDetailView) {
+                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework, isShowingDetailView: $viewModel.isShowingDetailView)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
---
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Steven Schwab on 12/15/22.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet { isShowingDetailView = true }
+    }
+    
+    @Published var isShowingDetailView = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+}
